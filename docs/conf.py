58a2ff830903b155caa01d0db5604653e1c7c81d
Spencer Jones <csjones@Spencers-MacBook-Air.local>
2020-05-05 13:59:08 -0400

    mock f2py

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.napoleon']
 
-autodoc_mock_imports = ["numpy"]
+autodoc_mock_imports = ["f2py","numpy"]
 
 
 # Add any paths that contain templates here, relative to this directory.
